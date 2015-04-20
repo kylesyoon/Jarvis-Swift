306e44b07ecc80867d9c06c5c7353b40ed1770c4
Kyle Yoon <kyle.yoon@vokalinteractive.com>
2015-04-20 16:47:50 -0500

    Initial Commit

---
+++ Jarvis-iOS-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Jarvis-iOS-Swift
+//
+//  Created by Kyle Yoon on 4/20/15.
+//  Copyright (c) 2015 Kyle Yoon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
